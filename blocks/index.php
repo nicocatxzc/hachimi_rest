c2efd18f93165a7618c3d90c9dc9f1b77ceb7106
nicocatxzc <nicocatxzc@163.com>
2025-12-31 22:52:30 +0800

    添加短代码

---
+++ blocks/index.php
@@ -0,0 +1,8 @@
+<?php
+add_shortcode('friend_link', function(){
+    return '<div id="hachimi-friend-link"></div>';
+});
+
+add_shortcode('bangumi', function(){
+    return '<div id="hachimi-bangumi"></div>';
+});
