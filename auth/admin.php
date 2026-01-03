bd18cefc438b2d1bed5ce80afa0b6532144cda75
nicocatxzc <nicocatxzc@163.com>
2026-01-03 19:29:06 +0800

    修改sso生成逻辑

--- auth/admin.php
+++ auth/admin.php
@@ -3,26 +3,41 @@
 add_filter('admin_bar_menu', function ($bar) {
     if (!is_user_logged_in()) return;
 
+    $node = $bar->get_node('view-site');
+
+    $href = admin_url('admin-post.php?action=hachimi_sso');
+
+    if ($node) {
+        $node->href = $href;
+        $bar->add_node($node);
+    } else {
+        $bar->add_node([
+            'id'    => 'hachimi-frontend',
+            'title' => '登录到前台',
+            'href'  => $href,
+        ]);
+    }
+}, 100);
+
+add_action('admin_post_hachimi_sso', function () {
+    if (!is_user_logged_in()) {
+        wp_die('Unauthorized', 403);
+    }
+
     $user = wp_get_current_user();
 
     // 生成一次性 code
     $code = hachimi_random_token(32);
     $key  = 'hachimi_sso_' . hachimi_hash($code);
 
-    $node = $bar->get_node('view-site');
     set_transient($key, [
-        'user_id' => $user->ID,
+        'user_id'   => $user->ID,
         'issued_at' => time(),
     ], 180); // 3 分钟
 
-    if ($node) {
-        $node->href = 'https://'.hachimi_get_setting('frontend_domain').'/sso?code=' . rawurlencode($code);
-        $bar->add_node($node);
-    } else {
-        $bar->add_node([
-            'id'    => 'hachimi-frontend',
-            'title' => '登录到前台',
-            'href'  => 'https://'.hachimi_get_setting('frontend_domain').'/sso?code=' . rawurlencode($code),
-        ]);
-    }
-}, 100);
+    $url = 'https://' . hachimi_get_setting('frontend_domain')
+        . '/sso?code=' . rawurlencode($code);
+
+    wp_redirect($url, 302);
+    exit;
+});
